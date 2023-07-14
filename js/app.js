ae4c401dc85a150c7d7d4c1f9843d6262c78f10f
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2023-07-13 21:08:56 -0300

    add função para pegar os valores da api e retornar ao usuario

--- js/app.js
+++ js/app.js
@@ -6,36 +6,49 @@ const prevAndNextContainer = document.querySelector("#prev-and-next-container")
 const apiURL = `https://api.lyrics.ovh`
 
 const getMoreSongs = async url => {
+
     const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
     const data = await response.json()
-
-    insertSongsIntoPage(data)
+    console.log(data)
+    insertSongsInotPage(data)
 }
 
-const insertSongsIntoPage = songsInfo => {
+const insertSongsInotPage = songsInfo => {
 
-    console.log(songsInfo)
     songsContainer.innerHTML = songsInfo.data.map(song => `
-    <li class="song">
-        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
-    </li>
+        <li class="song">
+            <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+        </li>
+    `).join('')
+
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+            ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+        return;
+    }
+
+
+    prevAndNextContainer.innerHTML = ''
 }
 
 const fetchSongs = async term => {
-    const response = await fetch(`${ apiURL } / suggest / ${ term }`)
+    const response = await fetch(`${apiURL}/suggest/${term}`)
     const data = await response.json()
 
-    insertSongsIntoPage(data)
+    insertSongsInotPage(data)
+
 }
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
+form.addEventListener('submit', e => {
+    e.preventDefault()
 
-    const searchTerm = searchInput.value.trim();
+    const searchTerm = searchInput.value.trim()
 
     if (!searchTerm) {
-        songsContainer.innerHTML = `< li class= "warning-messege" > Digite um termo válido</li > `
+        songsContainer.innerHTML = `<li class="warning-message">Por favor, digite um termo válido</li>`
         return;
     }
 
